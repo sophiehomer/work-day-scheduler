bf56443f68d4fc03a60e74fe042d24ea15cd02f0
sophiehomer <sophiehomer94@gmail.com>
2022-01-28 18:06:54 -0800

    local storage 9am-5pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,11 +11,29 @@ $(document).ready(function() {
          localStorage.setItem(time, description);
     })
    
-    let aValue = localStorage.getItem(time);
-    console.log(aValue)
-    // Use jquery syntax to target description in specific time block
-        // set value to local storage of corresponding hour
-        $("#9am .description").val(aValue);
+    let nineAm = localStorage.getItem("9am");
+    let tenAm = localStorage.getItem("10am");
+    let elevenAm = localStorage.getItem("11am");
+    let twelvePm = localStorage.getItem("12pm");
+    let onePm = localStorage.getItem("1pm");
+    let twoPm = localStorage.getItem("2pm");
+    let threePm = localStorage.getItem("3pm");
+    let fourPm = localStorage.getItem("4pm");
+    let fivePm = localStorage.getItem("5pm");
+
+    
+    /* ---- Use jquery syntax to target description in specific time block --- */
+        /* ----------- set value to local storage of corresponding hour ---------- */
+        $("#9am .description").val(nineAm);
+        $("#10am .description").val(tenAm);
+        $("#11am .description").val(elevenAm);
+        $("#12pm .description").val(twelvePm);
+        $("#1pm .description").val(onePm);
+        $("#2pm .description").val(twoPm);
+        $("#3pm .description").val(threePm);
+        $("#4pm .description").val(fourPm);
+        $("#5pm .description").val(fivePm);
+
 })
 
 /* ---------------------------------- DATE ---------------------------------- */
