fbb001939f7018e318d936f2cf304338f9fb2ff6
sophiehomer <sophiehomer94@gmail.com>
2022-01-28 17:17:03 -0800

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,46 @@
-// Variables
+
+$(document).ready(function() {
+    let time;
+    $(".saveBtn").on("click", function() {
+         /* ------------------------------ Variables ------------------------------ */
+         time = $(this).parent().attr("id")
+        console.log(time)
+        let description = $(this).siblings(".description").val()
+        console.log(description)
+        /* ---------------------------- Local Storage ---------------------------- */
+         localStorage.setItem(time, description);
+    })
+   
+    let aValue = localStorage.getItem(time);
+    console.log(aValue)
+    // Use jquery syntax to target description in specific time block
+        // set value to local storage of corresponding hour
+        $("#9am .description").val(aValue);
+})
+
+/* ---------------------------------- DATE ---------------------------------- */
+let currentDay;
+currentDay = new Date();
+document.querySelector("#currentDay").innerHTML = currentDay
+console.log(currentDay.toString());
 
 
-// Date
 
 
-// Textarea Background Color - Past, Present, Future
 
 
-// Time Blocks
 
+/* --------------- Background Color - Past, Present, Future --------------- */
 
-// Time Block Event
+/* ------------------------------- Textarea ------------------------------ */
 
+/* ----------------------------- Time Blocks ----------------------------- */
 
-// Save Button
+/* --------------------------- Time Block Event -------------------------- */
 
+/* ----------------------------- Save Button ----------------------------- */
 
-// Local Storage
-
-
-// Refresh -- Saved Events Persist
+/* ------------------- Refresh -- Saved Events Persist ------------------- */
 
 
 
